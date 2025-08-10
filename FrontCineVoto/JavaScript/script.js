1da746be489613aafb6da6a0f77af6e8c3bc70ff
manunasci <nascimentomanuella73@gmail.com>
2025-08-09 23:42:53 -0300

    Adicionando o Frontend completo

---
+++ FrontCineVoto/JavaScript/script.js
@@ -0,0 +1,9 @@
+// Lógica do menu hambúrguer
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const navLinks = document.querySelector('.nav-links');
+
+menuHamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Outras funções globais
